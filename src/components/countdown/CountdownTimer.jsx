915610497a56e513b71afc606375100a2d5e3481
Marcelo Barsotti <mbarsott@gmail.com>
2023-04-16 15:17:33 -0400

    Remove declarations that were breaking the build

--- src/components/countdown/CountdownTimer.jsx
+++ src/components/countdown/CountdownTimer.jsx
@@ -1,20 +1,13 @@
-import React, { useState } from "react";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
-import {
-  MdOutlineNotStarted,
-  MdOutlinePauseCircleOutline,
-  MdOutlineStopCircle,
-} from "react-icons/md";
+import React, { useState } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { MdOutlineNotStarted, MdOutlinePauseCircleOutline, MdOutlineStopCircle } from 'react-icons/md';
 
 const CountdownTimer = ({ pageTitle }) => {
   const secondsInOneHour = 3600;
   const secondsInOneMinute = 60;
 
-  const primaryColor = "#ea605e";
-  const secondaryColor = "#151932";
-  const tertiaryColor = "#80b895";
-  const lightColor = "#f8f9fa";
-  const darkBlueColor = "#0c0e1b";
+  const primaryColor = '#ea605e';
+  const darkBlueColor = '#0c0e1b';
 
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -28,7 +21,7 @@ const CountdownTimer = ({ pageTitle }) => {
       setTimerFinished(false);
       setIsRunning(true);
     } else {
-      window.alert("Please, add time");
+      window.alert('Please, add time');
     }
   }
   function pauseTimer() {
@@ -46,11 +39,11 @@ const CountdownTimer = ({ pageTitle }) => {
   }
 
   const totalSeconds =
-    hours !== "" && minutes !== "" && seconds !== ""
+    hours !== '' && minutes !== '' && seconds !== ''
       ? hours * secondsInOneHour + minutes * secondsInOneMinute + seconds
       : 1;
 
-  const toStringWithTwoDigits = (n) => (n > 9 ? "" + n : "0" + n);
+  const toStringWithTwoDigits = (n) => (n > 9 ? '' + n : '0' + n);
 
   const formatRemainingTime = ({ remainingTime }) => {
     let time = remainingTime;
@@ -60,85 +53,71 @@ const CountdownTimer = ({ pageTitle }) => {
     time -= minutes * secondsInOneMinute;
     const seconds = time % secondsInOneMinute;
 
-    return (
-      toStringWithTwoDigits(hours) +
-      ":" +
-      toStringWithTwoDigits(minutes) +
-      ":" +
-      toStringWithTwoDigits(seconds)
-    );
+    return toStringWithTwoDigits(hours) + ':' + toStringWithTwoDigits(minutes) + ':' + toStringWithTwoDigits(seconds);
   };
 
   return (
-    <div className="countdown-wrapper">
-      <div className="page-title">
+    <div className='countdown-wrapper'>
+      <div className='page-title'>
         <h1>{pageTitle}</h1>
       </div>
 
-      <div className="timer-controls">
-        <div className="inputs-wrapper">
-          <div className="timer-input">
+      <div className='timer-controls'>
+        <div className='inputs-wrapper'>
+          <div className='timer-input'>
             <label>hours</label>
             <input
-              type="number"
+              type='number'
               value={hours}
               onChange={(e) => setHours(+e.target.value)}
-              onFocus={() => setHours("")}
-              onBlur={() => (hours === "" ? setHours(0) : setHours(hours))}
+              onFocus={() => setHours('')}
+              onBlur={() => (hours === '' ? setHours(0) : setHours(hours))}
             />
           </div>
 
-          <div className="timer-input">
+          <div className='timer-input'>
             <label>minutes</label>
             <input
-              type="number"
+              type='number'
               value={minutes}
               onChange={(e) => setMinutes(+e.target.value)}
-              onFocus={() => setMinutes("")}
-              onBlur={() =>
-                minutes === "" ? setMinutes(0) : setMinutes(minutes)
-              }
+              onFocus={() => setMinutes('')}
+              onBlur={() => (minutes === '' ? setMinutes(0) : setMinutes(minutes))}
             />
           </div>
 
-          <div className="timer-input">
+          <div className='timer-input'>
             <label>seconds</label>
             <input
-              type="number"
+              type='number'
               value={seconds}
               onChange={(e) => setSeconds(+e.target.value)}
-              onFocus={() => setSeconds("")}
-              onBlur={() =>
-                seconds === "" ? setSeconds(0) : setSeconds(seconds)
-              }
+              onFocus={() => setSeconds('')}
+              onBlur={() => (seconds === '' ? setSeconds(0) : setSeconds(seconds))}
             />
           </div>
         </div>
 
-        <div className="countdown-buttons buttons-wrapper">
+        <div className='countdown-buttons buttons-wrapper'>
           {!isRunning && (
-            <button className="btn btn-green" onClick={startTimer}>
+            <button className='btn btn-green' onClick={startTimer}>
               <MdOutlineNotStarted />
             </button>
           )}
 
           {isRunning && (
-            <button className="btn btn-yellow" onClick={pauseTimer}>
+            <button className='btn btn-yellow' onClick={pauseTimer}>
               <MdOutlinePauseCircleOutline />
             </button>
           )}
 
-          <button className="btn btn-red" onClick={stopTimer}>
+          <button className='btn btn-red' onClick={stopTimer}>
             <MdOutlineStopCircle />
           </button>
         </div>
       </div>
 
-      <div
-        className={`timer-wrapper ${
-          timerFinished ? "bg-primary" : "bg-dark-blue"
-        }`}
-      >
+      <div className={`timer-wrapper ${timerFinished ? 'bg-primary' : 'bg-dark-blue'}`}>
         <CountdownCircleTimer
           key={key}
           isPlaying={isRunning}
@@ -146,7 +125,7 @@ const CountdownTimer = ({ pageTitle }) => {
           colors={primaryColor}
           strokeWidth={6}
           trailStrokeWidth={0}
-          strokeLinecap="round"
+          strokeLinecap='round'
           trailColor={darkBlueColor}
           onComplete={() => finishTimer()}
         >
--- src/components/pomodorosinglecircle/PomodoroSingleCircle.jsx
+++ src/components/pomodorosinglecircle/PomodoroSingleCircle.jsx
@@ -1,12 +1,9 @@
-import React, { useState, useRef, useEffect, useContext } from "react";
-import ReactSlider from "react-slider";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import {
-  MdOutlineNotStarted,
-  MdOutlinePauseCircleOutline,
-} from "react-icons/md";
-import { SettingsContext } from "../../context/SettingsContext";
+import React, { useContext } from 'react';
+import ReactSlider from 'react-slider';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { MdOutlineNotStarted, MdOutlinePauseCircleOutline } from 'react-icons/md';
+import { SettingsContext } from '../../context/SettingsContext';
 
 const PomodoroSingleCircle = ({ pageTitle }) => {
   const {
@@ -29,21 +26,21 @@ const PomodoroSingleCircle = ({ pageTitle }) => {
 
   return (
     <>
-      <div className="pomodoro-single-circle-wrapper">
-        <div className="page-title">
+      <div className='pomodoro-single-circle-wrapper'>
+        <div className='page-title'>
           <h1>{pageTitle}</h1>
         </div>
 
-        <div className="sliders-wrapper">
-          <div className="slider">
-            <label className="slider-label">
+        <div className='sliders-wrapper'>
+          <div className='slider'>
+            <label className='slider-label'>
               Focus: {focusMinutes}
-              {focusMinutes < 2 ? " minute" : " minutes"}
+              {focusMinutes < 2 ? ' minute' : ' minutes'}
             </label>
             <ReactSlider
-              className={"slider-bar slider-primary"}
-              thumbClassName={"thumb"}
-              trackClassName={"track"}
+              className={'slider-bar slider-primary'}
+              thumbClassName={'thumb'}
+              trackClassName={'track'}
               value={focusMinutes}
               onChange={(newValue) => setFocusMinutes(newValue)}
               min={0}
@@ -51,15 +48,15 @@ const PomodoroSingleCircle = ({ pageTitle }) => {
             />
           </div>
 
-          <div className="slider">
-            <label className="slider-label">
+          <div className='slider'>
+            <label className='slider-label'>
               Break: {breakMinutes}
-              {breakMinutes < 2 ? " minute" : " minutes"}
+              {breakMinutes < 2 ? ' minute' : ' minutes'}
             </label>
             <ReactSlider
-              className={"slider-bar slider-tertiary"}
-              thumbClassName={"thumb"}
-              trackClassName={"track"}
+              className={'slider-bar slider-tertiary'}
+              thumbClassName={'thumb'}
+              trackClassName={'track'}
               value={breakMinutes}
               onChange={(newValue) => setBreakMinutes(newValue)}
               min={0}
@@ -68,10 +65,10 @@ const PomodoroSingleCircle = ({ pageTitle }) => {
           </div>
         </div>
 
-        <div className="buttons-wrapper">
+        <div className='buttons-wrapper'>
           {isPaused ? (
             <button
-              className="btn btn-green"
+              className='btn btn-green'
               onClick={() => {
                 setIsPaused(false);
                 isPausedRef.current = false;
@@ -81,7 +78,7 @@ const PomodoroSingleCircle = ({ pageTitle }) => {
             </button>
           ) : (
             <button
-              className="btn btn-yellow"
+              className='btn btn-yellow'
               onClick={() => {
                 setIsPaused(true);
                 isPausedRef.current = true;
@@ -92,13 +89,13 @@ const PomodoroSingleCircle = ({ pageTitle }) => {
           )}
         </div>
 
-        <div className="progressbar-wrapper timer-wrapper">
+        <div className='progressbar-wrapper timer-wrapper'>
           <CircularProgressbar
             value={percentage}
-            text={minutes + ":" + seconds}
+            text={minutes + ':' + seconds}
             styles={buildStyles({
               textColor: lightColor,
-              pathColor: mode === "focus" ? primaryColor : tertiaryColor,
+              pathColor: mode === 'focus' ? primaryColor : tertiaryColor,
               trailColor: darkBlueColor,
               pathTransitionDuration: 0.1,
             })}
